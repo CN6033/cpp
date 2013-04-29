9f0e1e1280176b2793e6567e1f9c896a29fa9b51
huang shitao <apprentice1989@gmail.com>
2013-04-29 09:22:23 +0800

    Add a new  exercise code file

---
+++ tcpppl/ex-10-19.cc
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<string>
+#include<map>
+#include<vector>
+#include<sstream>
+#include<algorithm>
+
+using std::map;
+using std::string;
+using std::vector;
+using std::find;
+using std::cin;
+using std::istream;
+using std::istringstream;
+
+namespace TCPPPL{
+
+int func(const vector<string> words, istream &input, map<string, vector<int> > &word_lines_map)
+{
+    int line_num = 1;
+    string line, word;
+    while(getline(input, line)){
+        istringstream stream(line);
+        while(stream >> word){
+            //it has to be a const_iterator since words is a const vector<...>
+            vector<string>::const_iterator it = find(words.begin(), words.end(), word);
+            if(it != words.end()){
+                word_lines_map[word].push_back(line_num);
+            }
+        }
+        line_num++;
+    }
+}
+}
+
+int main()
+{
+    vector<string> words;
+    words.push_back("hello");
+    words.push_back("cpp");
+    words.push_back("world");
+    map<string, vector<int> > word_lines_map;
+
+    TCPPPL::func(words, cin, word_lines_map);
+}
